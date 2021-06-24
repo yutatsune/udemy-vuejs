c9c08ffefd3f5b58f94147c8eaaeff392cda1451
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-06-24 18:34:49 +0900

    global_component

---
+++ src/LikeNumber.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>いいね({{number}})</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 5
+    };
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import LikeNumber from "./LikeNumber.vue";
+/* ./LikeNumber.vueからインポートしている */
 
 Vue.config.productionTip = false
+Vue.component("LikeNumber", LikeNumber)
+/* Vue.component("名前", コンポーネント名)と書いてグローバル登録 */
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
