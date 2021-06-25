09c1dfe6aac4e68e952e9e4bcff50361db736ca8
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-06-25 17:35:23 +0900

    local_component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,25 @@
 <template>
-  <LikeNumber></LikeNumber>
-  <!-- ./LikeNumber.vueの内容を表示している -->
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+    <!-- ./LikeNumber.vueの内容を表示している -->
+  </div>
 </template>
+
+<script>
+import LikeHeader from "./LikeHeader.vue";
+
+export default {
+  components: {
+    LikeHeader
+  }
+};
+/* シングルファイルコンポーネントをローカル登録するには、
+import コンポーネント名 from "ファイルの場所";
+export default {
+  components: {
+    コンポーネント名
+  }
+};
+で読み込む */
+</script>
