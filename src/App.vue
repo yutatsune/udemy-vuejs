e11fb0e5c15dc58091a109c43b5fec05a90aff33
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-07-04 21:52:18 +0900

    slot

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
+    <LikeHeader>
+      <h1>トータルのいいね数</h1>
+      <h2>{{ number }}</h2>
+    </LikeHeader>
     <h2>{{ number }}</h2>
     <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
     <LikeNumber :total-number="number"></LikeNumber>
